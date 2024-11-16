cb4bb765238f293042d8341c93042798ee9e7c7b
Ryoma Shibata <r.shibata@cluster.mu>
2024-11-16 15:30:55 +0900

    fix code format

--- composeApp/src/androidMain/kotlin/com/kaeritei/kmpsandbox/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kaeritei/kmpsandbox/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
